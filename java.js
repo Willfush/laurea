c6cad4988716fc34e046fb3d1418539497d5e93f
Willfush <willfush2@gmail.com>
2024-12-06 23:59:26 +0100

    Add files via upload

--- java.js
+++ java.js
@@ -2,6 +2,26 @@ const button = document.getElementById("showVideoButton");
 const videoContainer = document.getElementById("videoContainer");
 const video = document.getElementById("myVideo");
 
+// Mostra il video e avvia in modalità schermo intero
+button.addEventListener("click", () => {
+    video.style.display = "block"; // Rendi visibile il video
+
+    video.pause(); // Reset del video
+    video.currentTime = 0; // Torna all'inizio del video
+    video.play(); // Avvia il video
+    enterFullscreen(video); // Entra in modalità fullscreen
+
+    // Controlla costantemente lo stato fullscreen
+    checkFullscreenInterval = setInterval(() => {
+        if (!isFullscreen()) { // Se si esce dal fullscreen
+            video.pause(); // Metti in pausa il video
+            video.currentTime = 0; // Resetta il video
+            video.style.display = "none"; // Nascondi il video
+            clearInterval(checkFullscreenInterval); // Ferma il controllo
+        }
+    }, 200);
+});
+
 let checkFullscreenInterval;
 
 // Funzione per entrare in modalità schermo intero
@@ -24,26 +44,11 @@ function isFullscreen() {
     );
 }
 
-// Mostra il video e avvia in modalità schermo intero
-button.addEventListener("click", () => {
-    videoContainer.style.display = "block"; // Mostra il contenitore del video
-    video.play(); // Avvia la riproduzione del video
-    enterFullscreen(video); // Entra in modalità fullscreen
-
-    // Controlla costantemente lo stato fullscreen
-    checkFullscreenInterval = setInterval(() => {
-        if (!isFullscreen()) { // Se esci dal fullscreen
-            videoContainer.style.display = "none"; // Nascondi il contenitore del video
-            video.pause(); // Metti in pausa il video
-            video.currentTime = 0; // Resetta il video
-            clearInterval(checkFullscreenInterval); // Ferma il controllo
-        }
-    }, 200);
-});
-
 // Reindirizza l'utente alla fine del video
-video.addEventListener('ended', function () {
+video.addEventListener("ended", function () {
     setTimeout(() => {
+        video.style.display = "none"; // Nasconde il video al termine
+        videoContainer.style.display = "none"; // Nasconde il contenitore
         window.location.href = "secondo.html"; // Cambia l'URL con quello desiderato
-    }, 1000); // Aspetta 1 secondo prima di reindirizzare
+    }, 1000);
 });

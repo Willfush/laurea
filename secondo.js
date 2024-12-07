30058106193d72b8e50328535becc835ccf25421
Willfush <willfush2@gmail.com>
2024-12-07 18:20:11 +0100

    Add files via upload

--- secondo.js
+++ secondo.js
@@ -1,5 +1,11 @@
 let Button = document.getElementById('button');
+let Mun = document.getElementById('serto')
+
 
 Button.addEventListener('click', function(){
     window.location.href = "index.html";
+})
+
+Mun.addEventListener('click', function(){
+    window.href = "youtube.com";
 })
